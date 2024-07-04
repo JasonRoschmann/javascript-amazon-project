3f0c91e6854d54616ca95f69f37bc52e45f8af0b
Jason Roschmann <jason.d.roschmann@gmail.com>
2024-07-04 13:30:51 +0200

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,22 @@
-export let cart = [{
-    productId: 
-    'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    
-    quantity: 2,
-}, {
-    productId: 
-    '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
-    quantity: 1
-}];
+if(!cart) {
+    cart = [{
+        productId: 
+        'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        
+        quantity: 2,
+    }, {
+        productId: 
+        '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    
+        quantity: 1
+    }];
+}
+
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId, selectedQuantity) {
     let matchingItem;
@@ -28,6 +36,8 @@ export function addToCart(productId, selectedQuantity) {
             quantity: selectedQuantity
         });
     }
+
+    saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -40,4 +50,6 @@ export function removeFromCart(productId) {
     });
 
     cart = newCart;
+
+    saveToStorage();
 }
